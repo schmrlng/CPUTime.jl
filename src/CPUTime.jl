f2c6d129a2eb396fefb39f938b17b381fa6d29df
Ed Schmerling <ednerd@gmail.com>
2014-06-26 16:35:56 -0700

    Initial version of CPUTime, support for CPU time analogues of base functions tic, toq, toc, @time, and @elapsed

--- src/CPUTime.jl
+++ src/CPUTime.jl
@@ -1,5 +1,64 @@
 module CPUTime
 
-# package code goes here
+export
+    CPUtime_us,
+    CPUtic,
+    CPUtoq,
+    CPUtoc,
+    @CPUtime,
+    @CPUelapsed
+
+function CPUtime_us()
+    rusage = c_malloc(4*sizeof(Clong) + 14*sizeof(Uint64))
+    ccall(:uv_getrusage, Uint64, (Ptr{Void},), rusage)
+    utime = 1000000*unsafe_load(convert(Ptr{Clong}, rusage + 0*sizeof(Clong))) + unsafe_load(convert(Ptr{Clong}, rusage + 1*sizeof(Clong)))  # user CPU time
+    stime = 1000000*unsafe_load(convert(Ptr{Clong}, rusage + 2*sizeof(Clong))) + unsafe_load(convert(Ptr{Clong}, rusage + 3*sizeof(Clong)))  # system CPU time
+    ttime = utime + stime  # total CPU time
+    c_free(rusage)
+    return ttime
+end
+
+function CPUtic()
+    t0 = CPUtime_us()
+    task_local_storage(:CPUTIMERS, (t0, get(task_local_storage(), :CPUTIMERS, ())))
+    return t0
+end
+
+function CPUtoq()
+    t1 = CPUtime_us()
+    timers = get(task_local_storage(), :CPUTIMERS, ())
+    if is(timers,())
+        error("CPUtoc() without CPUtic()")
+    end
+    t0 = timers[1]::Clong
+    task_local_storage(:CPUTIMERS, timers[2])
+    (t1-t0)/1e6
+end
+
+function CPUtoc()
+    t = CPUtoq()
+    println("elapsed CPU time: ", t, " seconds")
+    return t
+end
+
+# print elapsed CPU time, return expression value
+macro CPUtime(ex)
+    quote
+        local t0 = CPUtime_us()
+        local val = $(esc(ex))
+        local t1 = CPUtime_us()
+        println("elapsed CPU time: ", (t1-t0)/1e6, " seconds")
+        val
+    end
+end
+
+# print nothing, return elapsed CPU time
+macro CPUelapsed(ex)
+    quote
+        local t0 = CPUtime_us()
+        local val = $(esc(ex))
+        (CPUtime_us()-t0)/1e6
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,20 @@
 using CPUTime
 using Base.Test
 
+function add_and_sleep()
+    x = 0
+    for i in 1:1e7
+        x += i
+    end
+    sleep(1)
+end
+
+function time_difference()
+    tic()
+    CPUtic()
+    add_and_sleep()
+    return toq() - CPUtoq()
+end
+
 # write your own tests here
-@test 1 == 1
+@test 1.0 <= time_difference() <= 1.01
